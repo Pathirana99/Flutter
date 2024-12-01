a88efdb4d5d8a67104ea4a0f6383fee6269833ee
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2024-12-01 21:22:58 +0530

    Update main.dart

--- appl/lib/main.dart
+++ appl/lib/main.dart
@@ -1,60 +1,25 @@
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-      DevicePreview(
-        enabled: !kReleaseMode,
-        builder: (context) => MyApp(), // Wrap your app
-      ),
-  );
+void main(){
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
+      title: "Bdoor",
+      home: HomePage(),
     );
   }
 }
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
 
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Bdoor"),
-        backgroundColor: Color(0xFF02FFF7),
-        leading: IconButton(
-            onPressed: (){},
-            icon: Icon(Icons.menu)
-        ),
-        actions: [
-          IconButton(
-              onPressed: (){},
-              icon: Icon(Icons.search)
-          ),
-          IconButton(
-              onPressed: (){},
-              icon: Icon(Icons.more_vert)
-          ),
-        ],
-        flexibleSpace: Image.asset("assests/img1.jpg",
-        fit: BoxFit.cover,
-        ),
+        title: Text('Bdoor app'),
       ),
     );
   }
