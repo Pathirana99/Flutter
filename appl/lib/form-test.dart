5a4867201fd6c934e4d74c630fa2f523e92c09f1
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2024-12-13 00:16:01 +0530

    Update form-test.dart

--- appl/lib/form-test.dart
+++ appl/lib/form-test.dart
@@ -8,12 +8,60 @@ class FormTest extends StatefulWidget {
 }
 
 class _FormTestState extends State<FormTest> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  late String name;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.greenAccent,
         title: Text('Form - Tutorial 14'),
+      ),
+      body: SingleChildScrollView(
+        key: _formKey,
+        child: Form(
+          child: Container(
+            margin: const EdgeInsets.all(24.0),
+            child: Column(
+              children: [
+               TextFormField(
+                 maxLength: 20,
+                 decoration: InputDecoration(hintText: 'Name'),
+                 maxLines: 1,
+                 validator:(text){
+                   if(text!.isEmpty){
+                     return'can not empty';
+                   }
+                   return null;
+                 },
+                 onSaved: (text){
+                   name = text!;
+                 },
+               ),
+                SizedBox(
+                  height: 100,
+                ),
+                Container(
+                  child: ElevatedButton(
+                    onPressed: (){},
+                    // onPressed: () {
+                    //    if (_formKey.currentState!.validate()) {
+                    //      _formKey.currentState!.save();
+                    //     ScaffoldMessenger.of(context).showSnackBar(
+                    //       SnackBar(content: Text('Name saved: $name')),
+                    //     );
+                    //   }
+                    // },
+                   // child: Text('Save'),
+                    child: Text('Saved'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
