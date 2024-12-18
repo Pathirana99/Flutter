71782ecb205ed68900b7aca4be1ebc0300366ae7
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2024-12-18 18:22:52 +0530

    format

--- appl/lib/main.dart
+++ appl/lib/main.dart
@@ -9,7 +9,6 @@ import 'package:http/http.dart' as http;
 import 'models/Users.dart';
 import 'screens/detail.dart';
 
-
 void main() {
   runApp(
     DevicePreview(
@@ -34,12 +33,14 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
-      home: const Home(title: 'Flutter Builder',),
+      home: const Home(
+        title: 'Flutter Builder',
+      ),
     );
   }
 }
-class Home extends StatefulWidget {
 
+class Home extends StatefulWidget {
   const Home({Key? key, required this.title}) : super(key: key);
   final String title;
 
@@ -48,33 +49,32 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
-  Future<List<User>> getUsers() async{
+  Future<List<User>> getUsers() async {
     var url = Uri.parse('https://randomuser.me/api/?results=20');
     late http.Response response;
     List<User> users = [];
-    try{
+    try {
       response = await http.get(url);
-      if(response.statusCode == 200){
+      if (response.statusCode == 200) {
         Map peopleData = jsonDecode(response.body);
         List<dynamic> peoples = peopleData["results"];
 
-        for(var item in peoples){
+        for (var item in peoples) {
           var email = item['email'];
-          var name = item['name']['first']+" "+ item['name']['last'];
+          var name = item['name']['first'] + " " + item['name']['last'];
           var id = item['login']['uuid'];
           var avatar = item['picture']['large'];
           User user = User(id, name, email, avatar);
           users.add(user);
         }
-      }else{
+      } else {
         return Future.error("Somthing went wrong, ${response.statusCode}");
       }
-    }catch(e){
-       return Future.error(e.toString());
+    } catch (e) {
+      return Future.error(e.toString());
     }
     return users;
-}
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -84,41 +84,40 @@ class _HomeState extends State<Home> {
         title: Text("Asynchronous Programming"),
       ),
       body: FutureBuilder(
-          future: getUsers(),
-          builder: (BuildContext context, AsyncSnapshot snapshot){
-        if(snapshot.connectionState == ConnectionState.waiting){
-          return const Center(
-            child: Text('Waiting'),
-          );
-        }else{
-          if(snapshot.hasError){
-            return Text(snapshot.error.toString());
-          }else{
-            return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (BuildContext context, int index){
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage:
-                    NetworkImage(snapshot.data[index].avatar),
-                  ),
-                  title: Text(snapshot.data[index].name),
-                  subtitle: Text(snapshot.data[index].email),
-                  onTap: (){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            UserDetail(snapshot.data[index]),
-                      ),
-                    );
-                  },
-                );
-              }
+        future: getUsers(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: Text('Waiting'),
             );
+          } else {
+            if (snapshot.hasError) {
+              return Text(snapshot.error.toString());
+            } else {
+              return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage:
+                            NetworkImage(snapshot.data[index].avatar),
+                      ),
+                      title: Text(snapshot.data[index].name),
+                      subtitle: Text(snapshot.data[index].email),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                UserDetail(snapshot.data[index]),
+                          ),
+                        );
+                      },
+                    );
+                  });
+            }
           }
-        }
-      },
+        },
       ),
     );
   }
