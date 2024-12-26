6a833d682cf5a125aa3215d4dedd43bdc725b45f
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2024-12-26 13:22:10 +0530

    Update note_page.dart

--- appl/lib/screens/note_page.dart
+++ appl/lib/screens/note_page.dart
@@ -12,33 +12,11 @@ class _NoteState extends State<Note> {
   @override
   Widget build(BuildContext context) {
     return SecondPageModels(
-      title: 'Pg1',
+      title: 'Notes',
       content: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'This is Page 1',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Icon(Icons.star, color: Colors.yellow, size: 30),
-              SizedBox(width: 10),
-              Text(
-                'Centered Row - Pg1',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
-              ),
-            ],
-          ),
+
         ],
       ),
     );
